55c8ae41e2a0c1037990e0da6583a404bb8c9359
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-25 14:54:40 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -19,12 +19,36 @@
     for (let i = 0; i < targetElement.length; i++) {
       const getElementDistance =
         targetElement[i].getBoundingClientRect().top +
-        targetElement[i].clientHeight * 3;
+        targetElement[i].clientHeight * 2;
       if (window.innerHeight > getElementDistance) {
         targetElement[i].classList.add("show");
       }
     }
   });
+}
+
+{
+  function typing(str = ""){
+		let buf = document.getElementById("my_name").innerHTML;
+		let writed = buf.length;
+		let write = "";
+		if(writed < str.length){
+			write = str.charAt(writed);
+		}else{
+			// buf = "";
+      return;
+		}
+		document.getElementById("my_name").innerHTML = buf + write;
+	}
+
+	const str = document.getElementById("my_name").innerHTML;
+	const delay = 200;
+
+
+	document.getElementById("my_name").innerHTML = "";
+	window.setInterval(function(){typing(str);}, delay);
+
+  
 }
 
 
